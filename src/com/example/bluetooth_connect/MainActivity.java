5203235e3d9daa169afc562c9c2fcca85ba0f26e
Parag0892 <parag0892@gmail.com>
2015-08-09 10:11:37 +0530

    Start Discovery of Bluetooth Devices

--- src/com/example/bluetooth_connect/MainActivity.java
+++ src/com/example/bluetooth_connect/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.bluetooth_connect;
 import java.util.ArrayList;
 import java.util.Set;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -34,7 +38,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				
-				Toast.makeText(getApplicationContext(), "Clicked ", Toast.LENGTH_LONG).show() ;
+				Toast.makeText(getApplicationContext(), ">>Clicked ", Toast.LENGTH_LONG).show() ;
 				
 				
 				mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -52,6 +56,7 @@ public class MainActivity extends Activity {
 				}
 				else
 				{
+					System.out.println("inside else") ; 
 					devicesArray = mBluetoothAdapter.getBondedDevices();
 					
 					
@@ -84,33 +89,65 @@ public class MainActivity extends Activity {
 
 								@Override
 								public void onClick(DialogInterface dialog,int which) {
+									System.out.println(mBluetoothAdapter.startDiscovery()) ;
+									if (which != devicesArray.size() -1)
+									{
 									
-									Toast.makeText(getApplicationContext(), " which >>" + which, Toast.LENGTH_LONG) ; 
-									strName = arrayAdapter.getItem(which);
-									AlertDialog.Builder builderInner = new AlertDialog.Builder(MainActivity.this);
-									builderInner.setMessage(strName);
-									builderInner.setTitle("Your Selected Item is");
-									builderInner.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
-
-														@Override
-														public void onClick(DialogInterface dialog,int which) {
-															String add = strName.substring(strName.length() - 17);
-															Toast.makeText(getApplicationContext(),add, 2000).show();
-															dialog.dismiss();
-															
-														}
-													});
-									builderInner.show();
+										System.out.println("which >> " + which) ; 
+										strName = arrayAdapter.getItem(which);
+										AlertDialog.Builder builderInner = new AlertDialog.Builder(MainActivity.this);
+										builderInner.setMessage(strName);
+										builderInner.setTitle("Your Selected Item is");
+										builderInner.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+	
+															@Override
+															public void onClick(DialogInterface dialog,int which) {
+																String add = strName.substring(strName.length() - 17);
+																Toast.makeText(getApplicationContext(),add, 2000).show();
+																dialog.dismiss();
+																
+															}
+														});
+										builderInner.show();
+									}
+									else
+									{
+										System.out.println ("Pairup new devices") ; 
+										if (mBluetoothAdapter.isDiscovering()) {
+										    mBluetoothAdapter.cancelDiscovery();
+										}
+										
+										System.out.println(" start>>" + mBluetoothAdapter.startDiscovery()) ; 
+									}
 								}
 							});
 
-					
+					builderSingle.show() ;		
 				}
 			}		
 		}) ; 
 		
+		
+		BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		    public void onReceive(Context context, Intent intent) {
+		        String action = intent.getAction();
+		        // When discovery finds a device
+		        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+		            // Get the BluetoothDevice object from the Intent
+		            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+		            // Add the name and address to an array adapter to show in a ListView
+		            //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+		            System.out.println( "Device Name >> " + device.getName()) ;
+		        }
+		    }
+		};
+		
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(mReceiver, filter);
+		
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// TODO Auto-generated method stub
@@ -127,7 +164,7 @@ public class MainActivity extends Activity {
 			else if (resultCode == RESULT_OK) {
 				
 				Toast.makeText(getApplicationContext(), "Bluetooth has enabeled",2000).show();
-				pairup.performClick() ; 
+				pairup.callOnClick() ; 
 			
 			}
 		}
